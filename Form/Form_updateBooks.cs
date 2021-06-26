8f6cdca14190d8e48bbccb18c1eea28380828a35
YihuanZhou <1209201927@qq.com>
2021-06-26 11:16:50 +0800

    更新部分功能性注释

--- Form/Form_updateBooks.cs
+++ Form/Form_updateBooks.cs
@@ -16,7 +16,7 @@ namespace LibraryManagementSystem
     【功能】             向数据库更新图书信息的窗体
     【接口说明】         readNewInfo()、showOriInfo()、getBookInfo()、submit_Click(object sender, EventArgs e)、
     【开发者及日期】      Algernon, 2021/06/19
-    【版本】             1.0
+    【版本】             V1.0
     *************************************************************************/
     public partial class Form_updateBooks : Form
     {
@@ -28,6 +28,17 @@ namespace LibraryManagementSystem
         private string publisher;
         private string isBorrowed;
 
+        /***************************************************************
+        【函数名称】       Form_updateBooks
+        【函数功能】       构造函数、初始化窗体信息
+        【参数】           输入参数：string bno, userInfo theUser
+        【访问变量】       无
+        【返回值】         无
+        【使用情况】       调用InitializeComponent函数、getBookInfo函数、showOriInfo函数;被UCM_manageBooks::update_butt_Click函数调用
+        【开发者及日期】    Algernon, 2021/06/19
+        【版本】           V1.0
+        ****************************************************************/
+
         public Form_updateBooks(string bno, userInfo theUser)
         {
             InitializeComponent();
@@ -37,15 +48,34 @@ namespace LibraryManagementSystem
             showOriInfo();
         }
 
+        /***************************************************************
+        【函数名称】         readNewInfo
+        【函数功能】         读取输入的最新图书信息
+        【参数】             无
+        【访问变量】         tb_bname、tb_author、tb_pu、tb_ib
+        【返回值】           无
+        【使用情况】         未调用其他函数;被submit_Click函数调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void readNewInfo()
         {
-            // 更新信息
             bname = tb_bname.Text;
             author = tb_author.Text;
             publisher = tb_pu.Text;
             isBorrowed = tb_ib.Text;
         }
 
+        /***************************************************************
+        【函数名称】         showOriInfo
+        【函数功能】         将原图书信息写入窗口
+        【参数】             无
+        【访问变量】         bname、author、publisher、isBorrowed
+        【返回值】           无
+        【使用情况】         未调用其他函数;被Form_updateBooks构造函数调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void showOriInfo()
         {
             tb_bname.Text = bname;
@@ -53,7 +83,18 @@ namespace LibraryManagementSystem
             tb_pu.Text = publisher;
             tb_ib.Text = isBorrowed;
         }
+        
 
+        /***************************************************************
+        【函数名称】         getBookInfo
+        【函数功能】         读取原图书信息
+        【参数】             无
+        【访问变量】         OdbcConnection
+        【返回值】           无
+        【使用情况】         调用OdbcCommand::ExecuteReader函数;被Form_updateBooks构造函数调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void getBookInfo()
         {
             OdbcConnection con = new OdbcConnection(user.conString);
@@ -70,6 +111,17 @@ namespace LibraryManagementSystem
             }
         }
 
+
+        /***************************************************************
+        【函数名称】         submit_Click
+        【函数功能】         提交修改信息并反馈
+        【参数】             输入参数：object sender, EventArgs e
+        【访问变量】         OdbcConnection
+        【返回值】           无
+        【使用情况】         调用readNewInfo函数、OdbcCommand::Open、ExecuteNonQuery、Close函数、Dispose函数;被鼠标点击选中提交按钮动作所调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void submit_Click(object sender, EventArgs e)
         {
             readNewInfo();
--- Form/Form_whoBorrowedThisBook.cs
+++ Form/Form_whoBorrowedThisBook.cs
@@ -22,6 +22,17 @@ namespace LibraryManagementSystem
     {
         private string bno;
         private userInfo user;
+
+        /***************************************************************
+        【函数名称】         Form_whoBorrowedThisBook
+        【函数功能】         构造函数、初始化窗口信息
+        【参数】             输入参数：string bno, userInfo theUser
+        【访问变量】         无
+        【返回值】           无
+        【使用情况】         调用InitializeComponent函数、showInfo函数;被UCM_manageBooks::dgv_manageBooks_CellDoubleClick函数调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         public Form_whoBorrowedThisBook(string bno, userInfo theUser)
         {
             InitializeComponent();
@@ -32,6 +43,16 @@ namespace LibraryManagementSystem
 
         }
 
+        /***************************************************************
+        【函数名称】         showInfo
+        【函数功能】         在窗口中显示图书借阅信息
+        【参数】             无
+        【访问变量】         OdbcConnection
+        【返回值】           无
+        【使用情况】         调用OdbcCommand::Open、ExecuteReader函数;被Form_whoBorrowedThisBook构造函数调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void showInfo()
         {
             // 连接数据库
@@ -55,6 +76,16 @@ namespace LibraryManagementSystem
 
         }
 
+        /***************************************************************
+        【函数名称】         submit_Click
+        【函数功能】         确认信息后关闭窗口
+        【参数】             无
+        【访问变量】         无
+        【返回值】           无
+        【使用情况】         调用Dispose函数;被鼠标点击选中“ok”按钮动作所调用
+        【开发者及日期】      Algernon, 2021/06/19
+        【版本】             V1.0
+        ****************************************************************/
         private void submit_Click(object sender, EventArgs e)
         {
             this.Dispose();
